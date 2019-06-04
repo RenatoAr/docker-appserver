98cb81a4c39e963355b15488d0c0606c6fb5b1f7
Daniel Davoli <davolli@hotmail.com>
2019-06-04 14:59:23 -0300

    funcinando base

--- src/app.py
+++ src/app.py
@@ -1,15 +1,22 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    # provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '
+nome = 'nada'
+
+@app.route('/', methods=['GET'])
+def getinfo():
+    global nome
+    return 'Hello '+ nome
+
+@app.route('/', methods=['POST'])
+def postinfo():
+    global nome
+    nome = request.form['nome']
+    return 'hello' + nome
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
