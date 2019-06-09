a0277be9d813743ca4fb067ead805a939a2e64d0
renatoar <renato.rizzo@dcomp.sor.ufscar.br>
2019-06-09 02:49:00 -0300

    Corrigido request JSON

--- src/appserver.py
+++ src/appserver.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -15,9 +15,9 @@ def getinfo():
 
 @app.route('/', methods=['POST'])
 def postinfo():
-    dados = request.get_json()
-    print dados
     global dados
+    dados = request.get_json(force=True)
+    return jsonify(dados)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
